9dc9e6a32541b560c2c4764ba35475cdf492a91d
Luciano <lucianoamorim506@gmail.com>
2023-08-11 22:46:41 -0300

    feat: init

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,118 @@
+import { cardapio, metodosDePagamento } from './utils.js' ;
+
+class CaixaDaLanchonete {
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        const metodoDePagamentoValido = this.verificarMetodoDePagamento(metodoDePagamento);
+        if (!metodoDePagamentoValido.status) {
+            return metodoDePagamentoValido.mensagem;
+        }
+
+        itens = this.formatarItens(itens);
+        const itensValidos = this.verificarItens(itens);
+        if (!itensValidos.status) {
+            return itensValidos.mensagem;
+        }
+
+        const valorTotal = this.calcularTotal(itens);
+        const valorTotalFormatado = this.valorArredondadoETaxadoEFormatado(valorTotal, metodoDePagamentoValido);
+        
+ 
+        return valorTotalFormatado
+    }
+
+    calcularTotal(itens) {
+        return itens.reduce((total, item) => {
+            const itemCardapio = cardapio.find(cardapioItem => cardapioItem.codigo === item.nome);
+            return total + itemCardapio.valor * item.quantidade;
+        }, 0);
+    }
+
+    valorArredondadoETaxadoEFormatado(valor, metodoDePagamento) {
+
+        const valorTaxado = valor * metodoDePagamento.taxa;
+        const valorArredondado = valorTaxado.toFixed(2)
+        const valorFormatado = Number(valorArredondado).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+
+        return valorFormatado;
+    }
+
+    verificarItens(itens) {
+        let resultado = {
+            status: true,
+            mensagem: '',
+        }
+
+        if (itens.length < 1) {
+            resultado = {
+                status: false,
+                mensagem: 'Não há itens no carrinho de compra!',
+            }
+        }
+
+        itens.forEach(item => {
+            const itemCardapio = cardapio.find(cardapio => cardapio.codigo === item.nome);
+
+            if (!itemCardapio) {
+                resultado = {
+                    status: false,
+                    mensagem: 'Item inválido!',
+                }
+                return
+            }
+
+            if (item.quantidade < 1) {
+                resultado = {
+                    status: false,
+                    mensagem: 'Quantidade inválida!',
+                }
+                return
+            }
+
+            if (itemCardapio.condicao && !itens.some(item => item.nome === itemCardapio.condicao)) {
+                resultado = {
+                    status: false,
+                    mensagem: 'Item extra não pode ser pedido sem o principal',
+                }
+                return
+            }
+        })
+
+        return resultado
+    }
+
+    verificarMetodoDePagamento(metodoDePagamento) {
+        
+        const metodoDePagamentoSelecionado = metodosDePagamento.find(metodo => metodo.metodo === metodoDePagamento);
+        if (!metodoDePagamentoSelecionado) {
+            return {
+                status: false,
+                mensagem: 'Forma de pagamento inválida!',
+                taxa: 0,
+                metodo: '',
+            }
+        }
+    
+        return {
+            status: true,
+            mensagem: '',
+            taxa: metodoDePagamentoSelecionado.taxa,
+            metodo: metodoDePagamentoSelecionado.metodo,
+        }
+    }
+
+    formatarItens(itens) {
+        itens = itens.map(item => {
+            const [nome, quantidade] = item.split(',')
+            return {
+                nome,
+                quantidade: Number(quantidade),
+            }
+        })
+        return itens;
+    }
+}
+
+const caixaDaLanchonete = new CaixaDaLanchonete();
+console.log(caixaDaLanchonete.calcularValorDaCompra('credito', ['cafe,4', 'sanduiche,3', 'queijo,2']));
+
+export { CaixaDaLanchonete };
---
+++ src/utils.js
@@ -0,0 +1,69 @@
+
+const cardapio = [
+  {
+      codigo: "cafe",
+      valor : 3,
+      descricao: "Café",
+      condicao: undefined
+  },
+  {
+    codigo: "chantily",
+    valor : 1.50,
+    descricao: "Chantily (extra do Café)",
+    condicao: "cafe"
+  },
+  {
+    codigo: "suco",
+    valor : 6.20,
+    descricao: "Suco Natural",
+    condicao: undefined
+  },
+  {
+    codigo: "sanduiche",
+    valor : 6.50,
+    descricao: "Sanduíche",
+    condicao: undefined
+  },
+  {
+    codigo: "queijo",
+    valor : 2,
+    descricao: "Queijo (extra do Sanduíche)",
+    condicao: "sanduiche"
+  },
+  {
+    codigo: "salgado",
+    valor : 7.25,
+    descricao: "Salgado",
+    condicao: undefined
+  },
+  {
+    codigo: "combo1",
+    valor : 9.50,
+    descricao: "1 Suco e 1 Sanduíche",
+    condicao: undefined
+  },
+  {
+    codigo: "combo2",
+    valor : 7.50,
+    descricao: "1 Café e 1 Sanduíche",
+    condicao: undefined
+  }
+];
+
+const metodosDePagamento = [
+  {
+    metodo: "dinheiro",
+    taxa: 0.95
+  },
+  {
+    metodo: "credito",
+    taxa: 1.03
+  },
+  {
+    metodo: "debito",
+    taxa: 1
+  }
+];
+
+
+export {cardapio, metodosDePagamento};
